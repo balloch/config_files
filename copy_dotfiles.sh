574f7a277293e028617f93ba784d9aa57eb83e56
balloch <jon.balloch@gmail.com>
2020-05-23 16:33:05 -0400

    fixing some nameing and some git stuff

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -1,5 +1,6 @@
 cp .gitconfig ~/.gitconfig
 cp .gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 sudo cp tmux.conf /etc/tmux.conf
--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,6 +1,7 @@
 PWD={pwd}
 ln -s "$(pwd)/.gitconfig" ~/
 ln -s "$(pwd)/.gitignore_global" ~/
+git config --global core.excludesfile ~/.gitignore_global
 ln -s "$(pwd)/.vimrc" ~/ 
 ln -s "$(pwd)/.zshrc" ~/
 sudo ln -s "$(pwd)/tmux.conf" /etc/
